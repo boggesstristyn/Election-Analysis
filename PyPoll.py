f46dc18f8ce12583f862e1e6113ac103670e58b9
boggesstristyn <boggess.tristyn@gmail.com>
2022-05-08 12:34:17 -0500

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -63,20 +63,28 @@ with open(file_to_save,"w") as txt_file:
         vote_percentage = float(votes) / float(total_votes) * 100
         
         # To do: print out each candidates name, vote count, and percentage of votes to the terminal
-        #print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+        candidate_results = (f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+        # Print each candidate, their voter count, and percentage to the terminal
+        print(candidate_results)
+        # Save the candidate results to our text file
+        txt_file.write(candidate_results)
+
         # Determine winning vote count and candidate
         # Determine if the votes is greater than the winning count
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             # If true then the set winning_count = votes and winning_percent = vote_percentage
             winning_count = votes
-            winning_percentage = vote_percentage
             # And set the winning_candidate equal to the candidate's name
             winning_candidate = candidate_name
-        # Print wining candidates' results to the terminal
-        winning_candidate_summary = (
+            winning_percentage = vote_percentage
+
+    # Print wining candidates' results to the terminal
+    winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
-        #print(winning_candidate_summary)
+    print(winning_candidate_summary)
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
